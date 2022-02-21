eb057cff0e2833159c297ca9e7662c5d2dad8471
Michack Canive <55698720+MichakAntonio@users.noreply.github.com>
2022-02-21 08:47:05 +0100

    jb.news ,aleteração na icon react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { SubscribeButton } from '../Components/SubscribeButton';
 import styles from './home.module.scss';
-export default function Home() {
+export default function Home(props) {
+
     return (
         <>
             <Head>
@@ -9,19 +11,28 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Home | jb.news</title>
             </Head>
-           <main className={styles.contentContainer }>
-               <section className={styles.hero}>
-                <span>👋Olá, Bem-Vindo</span>
-                <h1>Notícias sobre o <span>React</span>no mundo.</h1>
-                <p>
-                Tenha acesso a todas as publicações <br/>
-                    <span className={styles.span_preso}>for $2.30 month</span>
-                </p>
-                <SubscribeButton/>
-               </section>
-               <img src='/images/avatar.png' alt='jb.news'/>
-           </main>
+            <main className={styles.contentContainer}>
+                <section className={styles.hero}>
+                    <span>👋Olá, Bem-Vindo</span>
+                    <h1>Notícias sobre o <span>React</span> no mundo.</h1>
+                    <p>
+                        Tenha acesso a todas as publicações <br />
+                        <span className={styles.span_preso}>por $2.30 mês</span>
+                    </p>
+                    <SubscribeButton />
+                </section>
+                <img src='/images/avatar.png' alt='jb.news' />
+            </main>
         </>
 
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    
+    return {
+    props:{
+        nome: 'mechack'
+    }
+}
 }
